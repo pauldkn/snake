4be2b4c893be77aa84be7f9da683b24b9cdcac6c
Paul Dkn <pauldkn.code@gmail.com>
2020-01-23 18:20:56 +0100

    Done: moveSnake correctly

--- index.js
+++ index.js
@@ -10,14 +10,12 @@ function clearArea() {
   ctx.clearRect(0, 0, canvas.width, canvas.height);
 }
 
+// FUNCTIONS FOR THE SNAKE DECLARATION
 var snake = {
   direction: "R", // right side
   speed: 20,
   body: [
-    { x: 80, y: 100 },
-    // { x: 60, y: 100 },
-    // { x: 40, y: 100 },
-    // { x: 20, y: 100 },
+    { x: 80, y: 100 }
   ]
 };
 function drawSnake() {
@@ -33,55 +31,59 @@ function drawSnake() {
   }
 }
 
+// FUNCTIONS FOR THE FOOD
 var food = {
   x: Math.round(Math.random() * 19) * 20,
-  y: Math.round(Math.random() * 19) * 20
+  y: Math.round(Math.random() * 19) * 20,
+  isEaten: false,
 };
-function drawFood() {
+function drawFood(){
   ctx.fillStyle = "red";
   ctx.fillRect(food.x, food.y, 20, 20);
 }
+function newFood(){
+  food.isEaten === true ? ctx.fillRect(food.x, food.y) : "";
+}
+function eatFood(){
+  if (snake.body[0].x === food.x && snake.body[0].y === food.y) {
+    food.isEaten = true;
+    snake.body.push({});
+    console.log("MIAAAAM");
 
+    snake.body.length < 20 ? currentScore += 10 : currentScore += 20;
+
+    score.innerHTML = `${currentScore} points`;
+  }
+}
+
+// FUNCTION FOR THE SNAKE DIRECTION : N/E/S/W
 function snakeDirection() {
   document.onkeydown = function(event) {
     switch (event.keyCode) {
       case 38: // up arrow
         snake.direction = "T"; // top
-        // console.log("Direction North!");
+        console.log("Direction North!");
         break;
       case 40: // down arrow
         snake.direction = "B"; // bottom
-        // console.log("Direction South!");
+        console.log("Direction South!");
         break;
       case 37: // left arrow
         snake.direction = "L"; // left
-        // console.log("Direction West!");
+        console.log("Direction West!");
         break;
       case 39: // right arrow
         snake.direction = "R"; // right
-        // console.log("Direction East!");
+        console.log("Direction East!");
         break;
     }
   };
 }
-
 var score = document.getElementById("points");
 var currentScore = 0;
 
-function eatFood() {
-  if (snake.body[0].x === food.x && snake.body[0].y === food.y){
-    
-    currentScore += 10;
-    console.log("MIAAAAM");
 
-    score.innerHTML = `${currentScore} points`;
-    snake.body.push({x: 0, y:0});
-
-    console.log(snake.body);
-  }
-}
-
-// SNAKE EATS ITS OWN TAIL
+// FUNCTION TO CHECK IF THE SNAKE EATS ITS OWN TAIL
 function ouroboros() {
   for (let i = 1; i < snake.body.length; i++) {
     if (
@@ -89,81 +91,68 @@ function ouroboros() {
       snake.body[0].y === snake.body[i].y
     ) {
       console.log("OUROBOROS", snake.body);
-      alert("END OF THE GAME!");
+      snake.speed = 0;
     }
   }
 }
 
-// FUNCTION STOPGAME : DOESN'T WORK FOR NOOOOOOOW
-// function stopGame() {
-//   for (let i = 0; i < snake.body.length; i++) {
-//     if (
-//       snake.body[i].x < 0 ||
-//       snake.body[i].x > 380 ||
-//       snake.body[i].y < 0 ||
-//       snake.body[i].y > 400
-//     ) {
-//       snake.isOut = true;
-//       console.log("YOU LOOOOOSE !!!");
-//     }
-//   }
-// }
-
-function moveSnake(){
-    switch (snake.direction) {
-        case "T":
-            for (let i = snake.body.length === 1 ? snake.body.length : snake.body.length - 1; i > 0; i--) {
-              snake.body.length === 1 ? "" : snake.body[i].x = snake.body[i - 1].x;
-              snake.body.length === 1 ? "" : snake.body[i].y = snake.body[i - 1].y;
-              snake.body[0].y -= snake.speed;
-              snake.body[0].y < 0 ? (snake.body[0].y = 380) : "";
-            }
-          break;
-        case "B":
-            for (let i = snake.body.length === 1 ? snake.body.length : snake.body.length - 1; i > 0; i--) {
-              snake.body.length === 1 ? "" : snake.body[i].x = snake.body[i - 1].x;
-              snake.body.length === 1 ? "" : snake.body[i].y = snake.body[i - 1].y;
-              snake.body[0].y += snake.speed;
-              snake.body[0].y > 380 ? (snake.body[0].y = 0) : "";
-            }
-          break;
-        case "L":
-            for (let i = snake.body.length === 1 ? snake.body.length : snake.body.length - 1; i > 0; i--) {
-              snake.body.length === 1 ? "" : snake.body[i].x = snake.body[i - 1].x;
-              snake.body.length === 1 ? "" : snake.body[i].y = snake.body[i - 1].y;
-              snake.body[0].x -= snake.speed;
-              snake.body[0].x < 0 ? (snake.body[0].x = 380) : "";
-            }
-          break;
-        case "R":
-            for (let i = snake.body.length === 1 ? snake.body.length : snake.body.length - 1; i > 0; i--) {
-                snake.body.length === 1 ? "" : snake.body[i].x = snake.body[i - 1].x;
-                snake.body.length === 1 ? "" : snake.body[i].y = snake.body[i - 1].y;
-                snake.body[0].x += snake.speed;
-                snake.body[0].x > 380 ? (snake.body[0].x = 0) : "";
-              }
-          break;
+// FUNCTION MOVE SNAKE WITH
+function moveSnake() {
+  switch (snake.direction) {
+    case "T":
+      for (let i = snake.body.length === 1 ? snake.body.length : snake.body.length - 1; i > 0; i--){
+        snake.body.length === 1 ? "" : (snake.body[i].x = snake.body[i - 1].x);
+        snake.body.length === 1 ? "" : (snake.body[i].y = snake.body[i - 1].y);
+        snake.body[0].y < 0 ? (snake.body[0].y = 380) : "";
       }
-}
-
-function logBody(){
-  for (let i = 0; i < snake.body.length; i++){
-    console.log(snake.body[i]);
+      snake.body[0].y -= snake.speed;
+      break;
+    case "B":
+      for (let i = snake.body.length === 1 ? snake.body.length : snake.body.length - 1; i > 0; i--){
+        snake.body.length === 1 ? "" : (snake.body[i].x = snake.body[i - 1].x);
+        snake.body.length === 1 ? "" : (snake.body[i].y = snake.body[i - 1].y);
+        snake.body[0].y > 380 ? (snake.body[0].y = 0) : "";
+      }
+      snake.body[0].y += snake.speed;
+      break;
+    case "L":
+      for (let i = snake.body.length === 1 ? snake.body.length : snake.body.length - 1; i > 0; i--){
+        snake.body.length === 1 ? "" : (snake.body[i].x = snake.body[i - 1].x);
+        snake.body.length === 1 ? "" : (snake.body[i].y = snake.body[i - 1].y);
+        snake.body[0].x < 0 ? (snake.body[0].x = 380) : "";
+      }
+      snake.body[0].x -= snake.speed;
+      break;
+    case "R":
+      for (let i = snake.body.length === 1 ? snake.body.length : snake.body.length - 1; i > 0; i--){
+        snake.body.length === 1 ? "" : (snake.body[i].x = snake.body[i - 1].x);
+        snake.body.length === 1 ? "" : (snake.body[i].y = snake.body[i - 1].y);
+        snake.body[0].x > 380 ? (snake.body[0].x = 0) : "";
+      }
+      snake.body[0].x += snake.speed;
+      break;
   }
 }
 
+// FUNCTION TO CHECK THE POSITION OF THE SNAKE
+// function logBody() {
+//   for (let i = 0; i < snake.body.length; i++) {
+//     console.log(i, snake.body[i]);
+//   }
+// }
+
 function gameLoop() {
-  moveSnake();
   clearArea();
   snakeDirection();
-  drawSnake();
-  drawFood();
   eatFood();
+  drawFood();
+  moveSnake();
   ouroboros();
-  // stopGame();
-  // console.log("oooooooo");
-  // console.log(food);
+  drawSnake();
   // requestAnimationFrame(gameLoop);
-  logBody();
+  // logBody();
 }
-setInterval(gameLoop, 2000);
+setInterval(gameLoop, 100);
+// buttonEasy.onclick = setInterval(gameLoop, 300);
+// buttonMedium.onclick = setInterval(gameLoop, 100);
+// buttonPython.onclick = setInterval(gameLoop, 50);
